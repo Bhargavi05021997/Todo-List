bb044328bc1852436f2877f4615762ff586e1ce1
Bhargavi05021997 <mudigondabhargavi70@gmail.com>
2022-06-22 08:36:50 -0700

    added api in app comment

--- projects/shopping/src/app/app.component.ts
+++ projects/shopping/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { UserService } from './services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'shopping';
   list:any[] = [];
-  
-
-
+  Users:any[] = [];
+ 
   addTask(item:string){
-    this.list.push({name:item})
+    this.list.push({name: item});
   }
   deleteTask(i:any){
     this.list.splice(i,1)
+  }
+  
+constructor(private data:UserService){}
+ngOnInit():void{
+    this.data.GetUsers().subscribe(user=>this.Users=user);
   }
 }
---
+++ projects/shopping/src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable  } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpClient) {
+    
+   }
+   GetUsers():Observable<any[]>{
+     return this.http.get<any[]>('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8');
+   }
+  }
